4d8518e77c4dda2213bddb6e95feb2b328004a3a
abhiraj998 <155749835+abhiraj998@users.noreply.github.com>
2024-01-10 11:46:54 +0530

    Update main.py
    api key updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
  
  
 def ytdName(name):
-    api_key = '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'
+    api_key = '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'
     openai.api_key = api_key
     prompt_text = f"give TOP 3 alternatives of {name} ETF, strictly within the same sector as {name}, return the results as a list only and i need only ticker names"
     response = openai.ChatCompletion.create(
@@ -25,7 +25,7 @@ def ytdName(name):
  
  
 def Sector(sector):
-    api_key = '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'
+    api_key = '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'
     openai.api_key = api_key
     prompt_text = f"return only the sector name for {sector} ETF?"
     response = openai.ChatCompletion.create(
